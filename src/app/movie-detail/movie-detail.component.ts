0f2bb26b42b473f18159ce60015c260e5a5bd0ef
Edebo <akandeadebowale0@gmail.com>
2019-10-21 23:34:34 +0100

    refactoring movie-detail

--- src/app/movie-detail/movie-detail.component.ts
+++ src/app/movie-detail/movie-detail.component.ts
@@ -2,14 +2,39 @@ import { Component, OnInit } from '@angular/core';
 import {MoviesService} from '../services/movies/movies.service'
 import {ActivatedRoute} from '@angular/router'
 import {ToastrService} from '../services/common/toastr.service'
-
+import {Movie} from './movies.interface'
 @Component({
   selector: 'app-movie-detail',
   templateUrl: './movie-detail.component.html',
   styleUrls: ['./movie-detail.component.css']
 })
 export class MovieDetailComponent implements OnInit {
-  movie:{}
+  movie:any={
+    Title:'',
+Year: '',
+Rated:'',
+Released: '',
+Runtime: '',
+Genre: '',
+Director: '',
+Writer:'',
+Actors: '',
+Plot:'',
+Language:'',
+Country: '',
+Awards: '',
+Poster: '',
+Ratings: [],
+Metascore:'',
+imdbRating:'',
+imdbVotes:'',
+imdbID:'',
+Type:'',
+DVD:'',
+BoxOffice:'',
+Production:'',
+Website:'',
+Response:''}
   title:string
   success:boolean
   constructor(private moviesService:MoviesService,
@@ -19,7 +44,7 @@ export class MovieDetailComponent implements OnInit {
   ngOnInit() {
   this.moviesService.getMovie(this.route.snapshot.params['id'])
     .subscribe(data=>{
-          this.movie=data
+          this.movie =data
     },error=>{
       console.log(error)
     }
